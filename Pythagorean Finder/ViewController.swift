1eb9153e60c78dc65a17e6628ec2ff12fcd37089
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2021-06-29 21:12:38 +0200

    First draft of my Pythagorean Finder App

---
+++ Pythagorean Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pythagorean Finder
+//
+//  Created by Dorian Emenir on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
