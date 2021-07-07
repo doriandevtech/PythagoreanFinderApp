8c3166ff477529fe060bc59873243920bb3ffeef
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2021-07-07 19:11:00 +0200

    Building of the ResultViewController file

---
+++ Pythagorean Finder/Controller/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  Pythagorean Finder
+//
+//  Created by Dorian Emenir on 07/07/2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var resultValue: Int?
+    var color: UIColor?
+    
+    
+}
