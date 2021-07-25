d0c4508ef97ddeebc949031fa55f97e71289f6b2
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2021-07-25 19:53:26 +0200

    Main view update

--- Pythagorean Finder/Controller/InputViewController.swift
+++ Pythagorean Finder/Controller/InputViewController.swift
@@ -11,15 +11,36 @@ class InputViewController: UIViewController {
     
     var calculatorBrain = CalculatorBrain()
 
+    @IBOutlet weak var mainTitle: UILabel!
+    
+    @IBOutlet weak var firstSideTitle: UILabel!
     @IBOutlet weak var firstSideLabel: UILabel!
+    @IBOutlet weak var secondSideTitle: UIStackView!
     @IBOutlet weak var secondSideLabel: UILabel!
+    
     @IBOutlet weak var firstSideSlider: UISlider!
     @IBOutlet weak var secondSideSlider: UISlider!
+    
+    @IBOutlet weak var calculateTitle: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mainTitle.layer.cornerRadius = 15
+        mainTitle.layer.masksToBounds = true
+        
+        firstSideTitle.layer.cornerRadius = 15
+        firstSideTitle.layer.masksToBounds = true
+        firstSideLabel.layer.cornerRadius = 15
+        firstSideLabel.layer.masksToBounds = true
+        
+        secondSideTitle.layer.cornerRadius = 15
+        secondSideTitle.layer.masksToBounds = true
+        secondSideLabel.layer.cornerRadius = 15
+        secondSideLabel.layer.masksToBounds = true
+        
+        calculateTitle.layer.cornerRadius = 15
+        calculateTitle.layer.masksToBounds = true
     }
     
     @IBAction func firstSideChanged(_ sender: UISlider) {
