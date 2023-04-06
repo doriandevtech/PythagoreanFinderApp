bce866fe1f039168d574d521b0f7ddbf84aa683e
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-04-06 22:19:04 +0200

    Add: Documentation ✍️

--- Pythagorean Finder/Controller/InputViewController.swift
+++ Pythagorean Finder/Controller/InputViewController.swift
@@ -11,38 +11,42 @@ class InputViewController: UIViewController {
     
     var calculatorBrain = CalculatorBrain()
 
-    @IBOutlet weak var mainTitle: UILabel!
     
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var mainTitle: UILabel!
     @IBOutlet weak var firstSideTitle: UILabel!
     @IBOutlet weak var firstSideLabel: UILabel!
     @IBOutlet weak var secondSideTitle: UILabel!
     @IBOutlet weak var secondSideLabel: UILabel!
-    
     @IBOutlet weak var firstSideSlider: UISlider!
     @IBOutlet weak var secondSideSlider: UISlider!
-    
     @IBOutlet weak var calculateTitle: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup(mainTitle)
-
         setup(firstSideTitle)
         setup(firstSideLabel)
-        
         setup(secondSideTitle)
         setup(secondSideLabel)
-        
         setup(firstSideSlider)
         setup(secondSideSlider)
-                
-        setup(calculateTitle)    }
+        setup(calculateTitle)
+    }
+
     
+    // MARK: setup UIView objects
+    
+    /// setup(_ object: UIView) : sets up the layer's cornerRadius and masksToBounds properties of any UIView object
+    /// - Parameter object: UIView object (e.g. UILabel, UISlider, UIButton)
     func setup(_ object: UIView) {
         object.layer.cornerRadius = 15
         object.layer.masksToBounds = true
     }
+    
+    
+    // MARK: IBActions
     
     @IBAction func firstSideChanged(_ sender: UISlider) {
         let firstVal = String(format: "%.0f", sender.value)
@@ -61,6 +65,9 @@ class InputViewController: UIViewController {
         calculatorBrain.calculateSide(firstSide: firstValue, secondSide: secondValue)
         performSegue(withIdentifier: "goToValue", sender: self)
     }
+
+    
+    // MARK: Segue
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToValue" {
